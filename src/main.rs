f1798886614b048f53eda10b41e20010ac7ec076
Jonah Weissman <jonahrweissman@gmail.com>
2021-10-23 15:04:08 -0400

    Use clap_app macro

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,34 @@
 use std::{fs::File, io};
 #[macro_use]
 extern crate clap;
-use anyhow::Result;
-use clap::{App, Arg};
+use anyhow::{Context, Result};
 use decide_config::{CorrectChoices, DecideConfig, Experiment};
 
 fn main() -> Result<()> {
-    let matches = App::new(crate_name!())
-        .version(crate_version!())
-        .about("Generate config files for chorus noise 2 alternative choice experiment")
-        .arg(
-            Arg::with_name("experiment")
-                .value_name("EXPERIMENT")
-                .help("yaml file containing a list of stimuli and parameters")
-                .required(true)
-                .index(1)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("correct")
-                .short("c")
-                .long("correct-choices-file")
-                .value_name("CORRECTCHOICES")
-                .default_value("correct_choices.yml")
-                .help("yaml file containing the correct choice for each stimulus")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("no-invert")
-                .short("i")
-                .long("no-inverted-config")
-                .help("skip generating an inverted answers config"),
-        )
-        .get_matches();
-    let experiment: Experiment =
-        serde_yaml::from_reader(File::open(matches.value_of("experiment").unwrap())?)?;
-    let correct_choices = match File::open(matches.value_of("correct").unwrap()) {
-        Ok(file) => serde_yaml::from_reader(file)?,
+    let matches = clap_app!(
+        @app (app_from_crate!())
+        (@arg experiment: <EXPERIMENT_YML> "yaml file containing stimuli, responses, and parameters")
+        (@arg correct: -c --("correct-choices") <CORRECT_YML>
+         !required "name for file with correct response for each stimulus")
+        (@arg no_invert: -i --("no-inverted-config") "skip generating an inverted answers config")
+    ).get_matches();
+    let experiment: Experiment = serde_yaml::from_reader(
+        File::open(matches.value_of("experiment").unwrap())
+            .context("could not open experiment file")?,
+    )
+    .context("could not parse experiment file")?;
+    let correct_choices_name = matches.value_of("correct").unwrap_or("correct_choices.yml");
+    let correct_choices = match File::open(correct_choices_name) {
+        Ok(file) => {
+            serde_yaml::from_reader(file).context("could not parse correct choices file")?
+        }
         Err(e) => {
             if let io::ErrorKind::NotFound = e.kind() {
                 let choices = CorrectChoices::random(&experiment)?;
-                let file = File::create(matches.value_of("correct").unwrap())?;
-                serde_yaml::to_writer(file, &choices)?;
+                let file = File::create(correct_choices_name)
+                    .context("could not create correct choices file")?;
+                serde_yaml::to_writer(file, &choices)
+                    .context("could not write correct choices file")?;
                 Ok(choices)
             } else {
                 Err(e)
@@ -52,12 +39,12 @@ fn main() -> Result<()> {
         let segmented = group
             .map(|g| format!("-segmented{}", g))
             .unwrap_or_else(|| "".into());
-        DecideConfig::from(&experiment, &correct_choices, true, group)?.to_json(format!(
+        DecideConfig::from(&experiment, &correct_choices, false, group)?.to_json(format!(
             "{}{}.json",
             experiment.get_name(),
             segmented
         ))?;
-        if !matches.is_present("no-invert") {
+        if !matches.is_present("no_invert") {
             DecideConfig::from(&experiment, &correct_choices, true, group)?.to_json(format!(
                 "{}-inverted{}.json",
                 experiment.get_name(),
